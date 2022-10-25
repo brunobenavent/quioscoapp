de90ed62b7401912ca491bf577a149946c40b1ec
brunitobc <70907703+brunitobc@users.noreply.github.com>
2022-10-26 01:56:34 +0200

    inicio del proyecto

---
+++ components/Categoria.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+
+const Categoria = ({ categoria }) => {
+  
+  return (
+    <div>
+        <h1>desde categoria</h1>
+    </div>
+  );
+};
+
+export default Categoria;
